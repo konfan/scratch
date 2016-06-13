bbe626006e7eb2a88533305d732413cc4c2a43e7
fan.03201@gmail.com <root@localhost.localdomain>
2016-06-13 18:19:55 +0800

    yoyo

--- python/danainstall/setuptool/core/executeplan.py
+++ python/danainstall/setuptool/core/executeplan.py
@@ -4,26 +4,41 @@ import fabric
 
 
 class Command(object):
-    def __init__(self, cmd, callback = None, title = None):
+    def __init__(self, cmd, hookmethod = None, title = None):
         self.title = title or ("Command: %s"%cmd)
         self.cmd = cmd
         self.callback = callback
         self.retcode = None
 
-    def run(self, target):
-        pass
+    #def execute(self, host):
+    #    return execute(self.__run, host = host)
+
+    def exec(self, host):
+        with settings(host_string = host):
+            v = run(self.cmd)
+
+        hook = hookmethod(v)
+        return hook()
+        
 
 
-class Target(object):
-    """
-    save host ip
-    """
-    def __init__(self, ip = None, roles = []):
-        self.host = ip
-        self.roles = roles
+class DefaultHook(object):
+    def __init__(self, runret):
+        self.ret = runret
 
+    def __call__(self):
+        if self.ret.succeeded:
+            return self.ret
 
-
+        else:
+            return ''
+#class Target(object):
+#    """
+#    save host ip
+#    """
+#    def __init__(self, hosts = None, roles = []):
+#        self.host = ip
+#        self.roles = roles
 
 
 
@@ -32,6 +47,5 @@ class ExecutePlan(object):
         self.commands = commands
         pass
 
-    
     def run():
         pass
